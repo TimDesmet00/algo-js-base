eb5474e1bf7fa015625256b3b6240a54ff564f17
tim <timdesmet00@gmail.com>
2023-09-08 16:59:29 +0200

    fin de journer dernier excercise js a finir

--- 2.7.js
+++ 2.7.js
@@ -1,5 +1,12 @@
-let n = parseInt(prompt("entrez un nombre"));
+let n = parseInt(prompt("entrez un nombre, n:"));
 let add = 0;
 
 if (!isNaN(n) && n > 0) {
+  for (let i = 0; i < n; i++) {
+    let num = parseInt(prompt("entrez nombre"));
+    if (!isNaN(num)) {
+      add += num;
+      console.log(add);
+    }
+  }
 }
