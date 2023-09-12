4d56d1c6e9b6ba7242b1a3a77bb9d6a36b66e282
tim <timdesmet00@gmail.com>
2023-09-12 12:15:17 +0200

    fin de l'exercice 5 avant le temps de midi

--- 5.2.js
+++ 5.2.js
@@ -1,1 +1,21 @@
-let randomizeCast = (tvSerie) => {};
+let randomizeCast = (castMembers) => {
+  let randomCast = [];
+  for (let i = 0; i < castMembers.length; i++) {
+    let randomIndex = Math.floor(Math.random() * castMembers.length);
+    randomCast.push(castMembers[randomIndex]);
+  }
+  return randomCast;
+};
+
+let randomizedCast = randomizeCast(mySerieTv.castMembers);
+console.log(randomizedCast);
+
+/*
+pour commencé j'ai crée la fonction randomizeCasr avec comme argument mon tableau de l'objet de l'exercice precedent.
+je crée un nouveau tableau ou stocker le nouvelle ordre de donnée.
+j'ai reutilisée ma boucle random de l'exercices 4.4, en modifiant les donnée par rapport au demande.
+comme i plus petit que le longueur du tableau de reference, le nouveau tableau ou ranger les donnée.
+je retourne le nouveau tableau.
+je crée une variable randomizedCast que je definit egale a randomizeCast avec l'argument du tableau dans mon objet.
+enfin je log dans la console la variable randomizedCast
+*/
